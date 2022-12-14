b3c50612d7d4400ced2be513e8996f08eb6c3978
Nathan Richards <nathan.richards@razor.co.uk>
2022-12-14 16:07:04 +0000

    relocated to composable

--- scripts/atoms/fontFamily.ts
+++
@@ -1,5 +0,0 @@
-export const fontFamily = {
-  sans: "'Segoe UI', Roboto, 'Helvetica Neue', Arial, 'Noto Sans', sans-serif",
-  serif: "ui-serif, Georgia, Cambria, 'Times New Roman', Times, serif",
-  mono: "ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, monospace",
-};
--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import { generateKey } from "./composables/generate-atoms";
+
+const keys = ["grid desktop", "test", "custom-grid", "600 -> 500 (90deg)"];
+
+keys.forEach((x) => console.log(generateKey(x)));
 </script>
 
 <template>
