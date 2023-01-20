136494e94eeed500ef83cb0fcbe5fba61c257675
Nathan Richards <nathan.richards@razor.co.uk>
2023-01-20 11:06:59 +0000

    Storing initial code - recursive is required

--- src/composables/atomGeneration/scripts/buildJSON.ts
+++ src/composables/atomGeneration/scripts/buildJSON.ts
@@ -5,6 +5,7 @@ export function buildJSON(object: Object): Object {
   const keyMethods = keyValueHelpers();
 
   const keysToRemove = [
+    "avatar user square",
     "blendMode",
     "description",
     "extensions",
@@ -18,11 +19,52 @@ export function buildJSON(object: Object): Object {
   }
 
   function build(): Object {
+    const newObject = {};
+
     // Remove unwanted keys
     const strippedObject = keyMethods.remove(object, keysToRemove);
 
     // Get all parent keys
     const parentKeys = keyMethods.get(strippedObject);
+
+    parentKeys.forEach((type) => {
+      newObject[type] = {};
+
+      for (const [key, value] of Object.entries(strippedObject[type])) {
+        // console.log(`${key}: ${value}`);
+        const path = keyMethods.findPath(strippedObject[type], "value");
+        const className = keyMethods.create(path);
+
+        console.log(className, key, value);
+
+        if (path === "base.white") {
+          newObject[type][className] = stripXChars(
+            strippedObject[type].base.white.value,
+            2
+          );
+        }
+      }
+
+      // Get class names with findPath
+      // Get value and assign
+
+      if (type === "color") {
+        // console.log("entries", Object.entries(strippedObject[type]));
+        // console.log("values", Object.values(strippedObject[type]));
+        // console.log("values top", Object.values(strippedObject));
+        // Object.entries(strippedObject).forEach(([key, value]) => {
+        //   console.log(value);
+        //   //console.log(keyMethods.findPath(strippedObject[type][key], "value"));
+        // });
+      }
+    });
+
+    console.log(newObject);
+
+    // Object.values(strippedObject).forEach((val) => {
+    //   console.log("value = ", val);
+    //   //console.log(keyMethods.findPath(val, "value"));
+    // });
 
     return parentKeys;
     // Use a loop method for each parent key
